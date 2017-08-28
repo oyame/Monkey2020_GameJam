5a8f64eac9d1aa0b25ebb4606fe2b4958fec01a1
DA\nwuser <daiki091548@gmail.com>
2017-08-28 15:00:39 +0900

    homing scripts

---
+++ Assets/Scripts/homing.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class homing : MonoBehaviour
+{
+
+    public Transform player;    //プレイヤーを代入
+    public float speed = 5; //移動速度
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = player.position;    //プレイヤーの位置
+        Vector3 direction = playerPos - transform.position; //方向
+        direction = direction.normalized;   //単位化（距離要素を取り除く）
+        transform.position = transform.position + (direction * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+    float speed=5;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // 右・左
+        float x = 0;
+
+        // 上・下
+        float y = Input.GetAxisRaw("Vertical");
+
+        // 移動する向きを求める
+        Vector2 direction = new Vector2(x, y).normalized;
+
+        // 移動する向きとスピードを代入する
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+}
