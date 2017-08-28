56435a3e67ca629644a9f6377bf35eaead6d3d89
oyama <oyameycry3024@gmail.com>
2017-08-28 14:59:47 +0900

    スプライト素材とりあえず入れました

---
+++ Assets/Scripts/Oyama/TestFootMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFootMove : MonoBehaviour {
+
+    public float maxRotateNum;
+
+    public float addRotateNum;
+
+    float unko = 0;
+
+	// Use this for initialization
+	void Start () {
+        unko = transform.eulerAngles.z;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        unko += addRotateNum;
+
+        transform.eulerAngles = new Vector3(0, 0, Mathf.Sin(unko * Mathf.Deg2Rad) * 50);
+
+        
+	}
+}
